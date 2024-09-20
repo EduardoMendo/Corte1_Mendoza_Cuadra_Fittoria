f5a1d6318851f2b6ce4dff54113dede9a5d496d3
EduardoMendo <efmendoza@uamv.edu.ni>
2024-09-19 18:52:15 -0600

    Mendoza

---
+++ Examen1C/src/main/java/Interfaces/Employable.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface Employable {
+    void addEmployee();
+    void showEmployeeInfo();
+}
--- Examen1C/src/main/java/org/efmb/Main.java
+++ Examen1C/src/main/java/org/efmb/Main.java
@@ -1,10 +1,67 @@
 package org.efmb;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Clerk clerk = null;
+        DeliveryPerson deliveryPerson = null;
+        Salesperson salesperson = null;
 
-        
+        int option;
+        do {
+            System.out.println("\n--- Sistema de Gestión de Empleados ---");
+            System.out.println("1. Agregar Oficinista");
+            System.out.println("2. Mostrar Información del Oficinista");
+            System.out.println("3. Agregar Repartidor");
+            System.out.println("4. Mostrar Información del Repartidor");
+            System.out.println("5. Agregar Comercial");
+            System.out.println("6. Mostrar Información del Comercial");
+            System.out.println("7. Salir");
+            System.out.print("Ingrese una opción: ");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    // Crear y añadir un oficinista
+                    break;
+                case 2:
+                    if (clerk != null) {
+                        clerk.showEmployeeInfo();
+                    } else {
+                        System.out.println("No hay oficinista registrado.");
+                    }
+                    break;
+                case 3:
+                    // Crear y añadir un repartidor
+                    break;
+                case 4:
+                    if (deliveryPerson != null) {
+                        deliveryPerson.showEmployeeInfo();
+                    } else {
+                        System.out.println("No hay repartidor registrado.");
+                    }
+                    break;
+                case 5:
+                    // Crear y añadir un comercial
+                    break;
+                case 6:
+                    if (salesperson != null) {
+                        salesperson.showEmployeeInfo();
+                    } else {
+                        System.out.println("No hay comercial registrado.");
+                    }
+                    break;
+                case 7:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, intente de nuevo.");
+                    break;
+            }
+        } while (option != 7);
+
+        scanner.close();
     }
 }
